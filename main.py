3de82d9d8250f1a454a7353fac4edafdc1b6328d
sjsone <simon@sjs.one>
2022-05-19 21:38:27 +0200

    Implemented Serial and new command from demo

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+import time
 from rigControl import RigControl
 
 
 
 def main():
     RigControl.sendInitializeInInterfaceCommand()
-    RigControl.sendTurnToCommand(60, 2)
-    RigControl.sendTurnToCommand(21.7, 2)
-    RigControl.sendTurnToCommand(0, 2)    
+    time.sleep(0.2)
+    # RigControl.sendTurnToCommand(60, 2)
+    # RigControl.sendTurnToCommand(21.7, 2)
+    # RigControl.sendTurnToCommand(0, 2)    
+
+    RigControl.sendTurnCommand(1)
+    time.sleep(3)
+    RigControl.sendTurnCommand(-1)
+    time.sleep(3)
+    RigControl.sendTurnCommand(0)
+    
 
 if __name__ == '__main__':
     main()
