0639984d327c0ab3142173bed6b9d24646d961d4
sjsone <simon@sjs.one>
2022-06-17 12:20:55 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,65 +1,35 @@
-from cgi import test
 import time
 from adapter.monkey import MonkeyAdapter
 from adapter.test import TestAdapter
 from adapter.warthunder import WTAdapter
 from rigControl.rigControl import RigControl
-import os
-from utils import getAdapters
- 
+import signal
+import sys
+
 
 def main():
-    # rigControl = RigControl("/dev/cu.usbserial-130")
-    rigControl = RigControl("/dev/cu.usbserial-140")
-    
+    rigControl = RigControl("/dev/cu.usbmodem1201")
     #rigControl.no_serial = True
 
     rigControl.init()
 
     rigControl.sendInitializeInInterfaceCommand()
-    time.sleep(5)
-    # rigControl.sendTurnCommand(0)
-
-    # time.sleep(15)
-
-    # print("wating...")
     # time.sleep(10)
 
+    adapter = WTAdapter(rigControl, "10.0.1.2")
+    adapter.start()
 
 
-    test = WTAdapter(rigControl)
-    test.start()
+    def signal_handler(sig, frame):
+        nonlocal adapter
+        print('Handling SIGINT (Ctrl+C)')
+        adapter.stop()
+        sys.exit()
 
-    # for i in range(2, 15, 1): 
-    # for i in map(lambda x: x/10.0, range(10, 150, 5)):
-        # print("TurnTo: ", i)
-        # rigControl.sendTurnToCommand(i, 4)
-        # time.sleep(2)
 
-    # for i in range(0, 10, 2): 
-    #     print("TurnTo: ", i)
-    #     rigControl.sendTurnCommand(i)
-    #     time.sleep(0.4)
-    # rigControl.sendTurnCommand(0)
 
-    # print("Will start")
-    # runAdapter(rigControl)
-    # runTest(rigControl)
-    # runTestAdapter(rigControl)
+    signal.signal(signal.SIGINT, signal_handler)
 
-def runAdapter(rigControl): 
-    monkeyAdapter = MonkeyAdapter(rigControl)
-    monkeyAdapter.start()
-
-def runTest(rigControl):
-    while True:
-        for i in range(10):
-            rigControl.sendTurnToCommand(i * 1, 5)
-            time.sleep(0.1)
-
-def runTestAdapter(rigControl):
-    testAdapter = TestAdapter(rigControl)
-    testAdapter.start()
     
 if __name__ == '__main__':
     main()
