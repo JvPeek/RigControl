e7c168a4c2478db0bca46e071e0082b1019d2f9f
Jan <janofthings@gmail.com>
2022-07-01 19:53:30 +0200

    added command line options

--- main.py
+++ main.py
@@ -7,29 +7,43 @@ from rigControl.rigControl import RigControl
 import signal
 import sys
 import paho.mqtt.client as paho
-
-broker="192.168.178.45"
-port=1883
-
-client1= paho.Client("control1")                           #create client object
-client1.connect(broker,port)                                 #establish connection
-client1.publish("house/bulb1","on")                   #publish
+import argparse
 
 
 
 
 def main():
-    rigControl = RigControl("/dev/ttyUSB0")
-    #rigControl.no_serial = True
+    parser = argparse.ArgumentParser(description='This is the bridge to receive data via UDP, parse it and send it to the brains of the rig.')
+    parser.add_argument('-p', '--port', default="/dev/ttyUSB0",
+                    help='Sets the port. Example: COM3. Default: /dev/ttyUSB0.')
+    parser.add_argument('-i', '--interface', default="127.0.0.1",
+                    help='Sets the interface to listen to. Default: 127.0.0.1')
+
+    parser.add_argument('--offline', default=False, action="store_true",
+                    help='Allows to run the app without brains attached.')
+    args = parser.parse_args()
+    print(args.port)
+    if (args.port == "none"):
+        rigControl = RigControl(None)
+        rigControl.no_serial = True
+    else:
+        rigControl = RigControl(args.port)
 
     rigControl.init()
 
-    adapter = MonkeyAdapter(rigControl, "192.168.178.48")
+    adapter = MonkeyAdapter(rigControl, args.interface)
     # adapter = WTAdapter(rigControl, "10.0.1.2")
 
     adapter.init()
     adapter.start()
 
+
+    # broker="192.168.178.45"
+    # port=1883
+    #
+    # client1= paho.Client("control1")                           #create client object
+    # client1.connect(broker,port)                                 #establish connection
+    # client1.publish("house/bulb1","on")                   #publish
 
     def signal_handler(sig, frame):
         nonlocal adapter
@@ -38,7 +52,6 @@ def main():
         sys.exit()
     signal.signal(signal.SIGINT, signal_handler)
 
-    
+
 if __name__ == '__main__':
     main()
-    
